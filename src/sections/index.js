2f731b289758a41a15b6f77e65479e74a71eafa1
DexFerrell <dexferrell@outlook.com>
2023-12-03 17:12:08 -0500

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+import Services from './services';
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  CustomerReviews,
+  SpecialOffer,
+  Subscribe,
+  Footer,
+  Services
+}
